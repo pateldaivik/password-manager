0a6e2ce5f8e25949f24ee5035f204518e9c425ec
Daivik <patel.daivik8@gmail.com>
2021-03-19 13:11:39 -0400

    Added search functionality

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 def generate_password():
@@ -25,15 +26,45 @@ def generate_password():
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def save():
+    new_data={web_input.get():{
+        "email":email_input.get(),
+        'password':password_input.get()
+        }
+    }
     if len(web_input.get())==0 or len(password_input.get()) ==0:
         messagebox.showinfo(title='Oops',message='Please make sure you have not left any fields empty.')
     else:
         is_ok = messagebox.askokcancel(title=web_input.get(),message=f"These are the details entered: \nEmail:{email_input.get()}\nPassword:{password_input.get()}\nIs it ok to save?")
         if is_ok:
-            with open('data.txt','a') as data:
-                data.write(f"{web_input.get()} | {email_input.get()} | {password_input.get()}\n")
-            web_input.delete(0,END)
-            password_input.delete(0,END)
+            try:
+                with open('data.json','r') as data:
+                    data_j = json.load(data)
+            except FileNotFoundError:
+                with open('data.json', 'w') as data:
+                    json.dump(new_data, data, indent=4)
+            else:
+                data_j.update(new_data)
+                with open('data.json', 'w') as data:
+                    json.dump(data_j,data,indent=4)
+            finally:
+                web_input.delete(0,END)
+                password_input.delete(0,END)
+
+
+def find_password():
+    website = web_input.get()
+    try:
+        with open('data.json') as data_file:
+            data=json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title='Error', message=f"No Data File Found")
+    else:
+        if website in data:
+            email=data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website,message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title='Error', message=f"No Details for {website} exists.")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -45,6 +76,7 @@ canvas = Canvas(height=200,width=200)
 logo_img = PhotoImage(file='logo.png')
 canvas.create_image(100,100,image=logo_img)
 canvas.grid(row=0,column=1)
+
 web_label = Label(text='Website:')
 web_label.grid(row=1,column=0)
 email_label = Label(text='Email/Username:')
@@ -53,17 +85,18 @@ password_label = Label(text='Password:')
 password_label.grid(row=3,column=0)
 
 
-web_input = Entry(width=35)
-web_input.grid(row=1,column=1,columnspan=2)
-email_input = Entry(width=35)
-email_input.grid(row=2,column=1,columnspan=2)
+web_input = Entry(width=21)
+web_input.grid(row=1,column=1)
+email_input = Entry(width=21)
+email_input.grid(row=2,column=1)
 email_input.insert(0,'daivik@gmail.com')
-password_input = Entry(width=35)
-password_input.grid(row=3,column=1,columnspan=2)
+password_input = Entry(width=21)
+password_input.grid(row=3,column=1)
 
-
-gen_pass = Button(text='Generate Password',width=30,command=generate_password)
-gen_pass.grid(row=4,column=1,columnspan=2)
-add = Button(text='Add',width=30, command=save)
-add.grid(row=5,column=1,columnspan=2)
+search_btn = Button(text='Search',width='13',command=find_password)
+search_btn.grid(row='1',column='2')
+gen_pass = Button(text='Generate Password',command=generate_password)
+gen_pass.grid(row=3,column=2)
+add = Button(text='Add',width=36, command=save)
+add.grid(row=4,column=1,columnspan=2)
 window.mainloop()
